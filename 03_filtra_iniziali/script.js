8d44318366abdc8a450b9e025263b917288a5ae1
maurizio-dominici <mauriziodominici4@gmail.com>
2025-04-08 16:38:38 +0200

    finish ex 03

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,13 +1,25 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+const serchedChar = "A";
 
 // Dichiara la funzione qui.
+function filterByInitials(namelist, serchedInitial) {
+  const filteredNames = [];
 
+  for (let i = 0; i < namelist.length; i++) {
+    const currentNmae = namelist[i];
+
+    if (currentNmae[0] === serchedInitial) {
+      filteredNames.push(currentNmae);
+    }
+  }
+
+  return filteredNames;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+filterByInitials(names, serchedChar);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+console.log(filterByInitials(names, serchedChar));
